f6e348f99d2512c1a44a2ab59ba3ae34c170df8b
Zahida313 <zahidaasghar.k@gmail.com>
2024-10-30 18:43:11 +0500

    Add files via upload
    Q# 01 Write a program that calculates the sum, difference, product, and average of three numbers:
    
    
    Q# 02 Write Python code to calculate the area of the following shapes:
    
    Area=0.5​×base×height
    Area=sidexside
    Area=π×radius2
    Area=length×width

---
+++ firstclass.py
@@ -0,0 +1,56 @@
+#Q no 1: calculate the sum, product, difference and average.
+#sum of numbers="2,4 and 6"
+sum1=2            
+sum2=4            
+sum3=6            
+add=(sum1+sum2+sum3)
+print("sum_result",add)   #12
+
+#difference of numbers="3,5 and 10"
+difference_1=3          
+difference_2=5           
+difference_3=10          
+subtract=(difference_1-difference_2-difference_3)
+print("difference_result",subtract)    #-12
+
+#multiply_of_numbers="3,5 and10"
+product_1=3           
+product_2=5            
+product_3=10           
+multiply=(product_1*product_2*product_3)
+print("multiply_result",multiply)   #150
+
+#average_of_numbers="2,6 and10"
+add1=2            
+add2=6            
+add3=10          
+average=(add1+add2+add3)/3
+print("average_result",average)   #6
+
+#Q:no2 :  write python code calculate the area of the following:
+
+#area of triangle = "0.5*b*h"
+base=2          
+height=10       
+area=(0.5*base*height)
+print("triangle_area",area)  #10
+
+#area of square ="s*s"
+side=4          
+area=(side*side)
+print("area of square",area)   #16
+
+#area of rectangle="l*h"
+length=4          
+width=2         
+area=(length*width)
+print("area of rectangle",area)    #8
+
+#area of circle ="pi*r*r"
+radius=10         
+pi=3.14           
+area=(pi*radius*radius)
+print("area of circle",area)   #314
+
+
+
